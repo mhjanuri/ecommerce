65999097a2ce47bfa63ef9710f0c0ae99d40649e
hendrijanuri <hendrijanuri@gmail.com>
2020-06-18 06:15:17 +0700

    init

---
+++ back/app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+// App
+const app = express();
+
+// DB
+mongoose
+    .connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log('DB Connected'));
+
+mongoose.connection.on('error', err => {
+    console.log(`DB connection error: ${err.message}`)
+});
+
+// Routes
+app.get('/', (req, res) => {
+    res.send('hello from the backend side');
+});
+
+const PORT = process.env.PORT || 2020;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port: ${PORT}`);
+});
