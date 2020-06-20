8970faa40b57c0b88a0e642e87e1bd79370125f4
hendrijanuri <hendrijanuri@gmail.com>
2020-06-20 19:55:24 +0700

    signout

--- back/routes/user.js
+++ back/routes/user.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 // Controllers
-const { signup, signin } = require('../controllers/user');
+const { signup, signin, signout } = require('../controllers/user');
 const { userSignupValidator } = require('../validator');
 
 // Routes
 router.post('/signup', userSignupValidator, signup);
 router.post('/signin', signin);
+router.get('/signout', signout);
 
 module.exports = router;
