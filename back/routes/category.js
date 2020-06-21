439bd4148d3ddbe01d101c0b87f2d773dbcd372c
hendrijanuri <hendrijanuri@gmail.com>
2020-06-21 23:30:14 +0700

    create category

--- back/routes/category.js
+++ back/routes/category.js
@@ -3,9 +3,14 @@ const router = express.Router();
 
 // Controllers
 const { create } = require('../controllers/category');
+const { requireSignin, isAuth, isAdmin } = require('../controllers/auth')
+const { userById } = require('../controllers/user');
 
 // Routes
-router.post('/category/create', create);
+router.post('/category/create/:userId', requireSignin, isAuth, isAdmin, create);
 
+
+router.param("userId", userById);
 
 module.exports = router;
+
