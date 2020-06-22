3960c9fb8b6c7d8a0146e0591eee97f3f9099f48
hendrijanuri <hendrijanuri@gmail.com>
2020-06-23 04:23:26 +0700

    user update

--- back/routes/user.js
+++ back/routes/user.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 // controllers
 const { requireSignin, isAuth, isAdmin } = require('../controllers/auth')
-const { userById } = require('../controllers/user');
+const { userById, read, update } = require('../controllers/user');
 
 // routes
 router.get('/secret/:userId', requireSignin, isAuth, isAdmin, (req, res) => {
@@ -11,6 +11,9 @@ router.get('/secret/:userId', requireSignin, isAuth, isAdmin, (req, res) => {
         user: req.profile
     });
 });
+
+router.get('/user/:userId', requireSignin, isAuth, read)
+router.put('/user/:userId', requireSignin, isAuth, update)
 
 router.param("userId", userById);
 
