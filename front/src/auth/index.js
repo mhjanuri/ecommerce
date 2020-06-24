10cb321bc06a41d0d84d0bee3c7c18f286912f20
hendrijanuri <hendrijanuri@gmail.com>
2020-06-25 00:25:43 +0700

    signup refac

---
+++ front/src/auth/index.js
@@ -0,0 +1,20 @@
+import { API_URL } from '../config';
+
+
+export const signup = user => {
+    // console.log(name, email, password);
+    return fetch(`${API_URL}/signup`, {
+        method: "POST",
+        headers: {
+            Accept: 'application/json',
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(user)
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => {
+            console.log(err);
+        });
+};
