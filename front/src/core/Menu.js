1f154fd62f0c553ee5cb48ff07d4f1c7203c3399
hendrijanuri <hendrijanuri@gmail.com>
2020-06-25 16:55:07 +0700

    private route

--- front/src/core/Menu.js
+++ front/src/core/Menu.js
@@ -1,6 +1,6 @@
 import React, { Fragment } from 'react';
 import { Link, withRouter } from 'react-router-dom';
-import { signout, isAuthenticated } from '../auth'
+import { signout, isAuthenticated } from '../auth';
 
 const isActive = (history, path) => {
     if (history.location.pathname === path) {
@@ -16,6 +16,12 @@ const Menu = ({ history }) => (
             <li className="nav-item">
                 <Link className="nav-link" style={isActive(history, '/')} to="/">
                     Home
+                </Link>
+            </li>
+
+            <li className="nav-item">
+                <Link className="nav-link" style={isActive(history, '/dashboard')} to="/dashboard">
+                    Dashboard
                 </Link>
             </li>
 
