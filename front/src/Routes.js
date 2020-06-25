f22fdd901424901be162647ee02b4783c488b872
hendrijanuri <hendrijanuri@gmail.com>
2020-06-25 21:11:53 +0700

    admin dashboard private route

--- front/src/Routes.js
+++ front/src/Routes.js
@@ -5,6 +5,10 @@ import Signup from './user/Signup';
 import Signin from './user/Signin';
 import PrivateRoute from './auth/PrivateRoute';
 import Dashboard from './user/UserDashboard';
+import AdminRoute from './auth/AdminRoute';
+import AdminDashboard from './user/AdminDashboard';
+
+
 
 const Routes = () => {
     return (
@@ -14,6 +18,8 @@ const Routes = () => {
                 <Route path="/signin" exact component={Signin} />
                 <Route path="/signup" exact component={Signup} />
                 <PrivateRoute path='/user/dashboard' exact component={Dashboard} />
+                <AdminRoute path='/admin/dashboard' exact component={AdminDashboard} />
+
             </Switch>
         </BrowserRouter>
     );
