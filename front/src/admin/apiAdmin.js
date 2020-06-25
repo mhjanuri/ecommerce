450ea95ec1771fddaca831f1e1847d4fc97b1bc8
hendrijanuri <hendrijanuri@gmail.com>
2020-06-26 02:11:47 +0700

    add product page

--- front/src/admin/apiAdmin.js
+++ front/src/admin/apiAdmin.js
@@ -18,3 +18,21 @@ export const createCategory = (userId, token, category) => {
             console.log(err);
         });
 };
+
+export const createProduct = (userId, token, product) => {
+    // console.log(name, email, password);
+    return fetch(`${API_URL}/product/create/${userId}`, {
+        method: "POST",
+        headers: {
+            Accept: 'application/json',
+            Authorization: `Bearer ${token}`
+        },
+        body: product
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => {
+            console.log(err);
+        });
+};
