2040e6f0efe04b3d7a964584912f86c9e7f27b7a
hendrijanuri <hendrijanuri@gmail.com>
2020-06-26 22:44:06 +0700

    add product form

--- front/src/admin/AddProduct.js
+++ front/src/admin/AddProduct.js
@@ -5,8 +5,128 @@ import { Link } from 'react-router-dom';
 import { createProduct } from './apiAdmin';
 
 const AddProduct = () => {
+    const [values, setValues] = useState({
+        name: '',
+        description: '',
+        price: '',
+        categories: [],
+        category: '',
+        shipping: '',
+        quantity: '',
+        photo: '',
+        loading: '',
+        error: '',
+        createdProduct: '',
+        redirectToProfile: false,
+        formData: ''
+    });
+
+    const {
+        name,
+        description,
+        price,
+        categories,
+        category,
+        shipping,
+        quantity,
+        loading,
+        error,
+        createdProduct,
+        redirectToProfile,
+        formData
+    } = values;
+
+    useEffect(() => {
+        setValues({ ...values, formData: new FormData() })
+    }, []);
+
+    const handleChange = name => event => {
+        const value = name === 'photo' ? event.target.files[0] : event.target.value;
+        formData.set(name, value);
+        setValues({ ...values, [name]: value });
+    };
+
+    const newPostForm = () => (
+        <form className="mb-3">
+            <h4>Post Photo</h4>
+            <div className="form-group">
+                <label className="btn btn-secondary">
+                    <input
+                        onChange={handleChange('photo')}
+                        type="file"
+                        name="photo"
+                        accept="image/*"
+                    />
+                </label>
+            </div>
+
+            <div className="form-group">
+                <label className="text-muted">Name</label>
+                <input
+                    onChange={handleChange('name')}
+                    type="text"
+                    className="form-control"
+                    value={name}
+                />
+            </div>
+
+            <div className="form-group">
+                <label className="text-muted">Description</label>
+                <textarea
+                    onChange={handleChange('description')}
+                    className="form-control"
+                    value={description}
+                />
+            </div>
+
+            <div className="form-group">
+                <label className="text-muted">Price</label>
+                <input
+                    onChange={handleChange('price')}
+                    type="number"
+                    className="form-control"
+                    value={price}
+                />
+            </div>
+
+            <div className="form-group">
+                <label className="text-muted">Category</label>
+                <select
+                    onChange={handleChange('category')}
+                    className="form-control"
+                >
+                    <option value="5ef4cda2305ff1395879349b">Python</option>
+                </select>
+            </div>
+
+            <div className="form-group">
+                <label className="text-muted">Shipping</label>
+                <select
+                    onChange={handleChange('shipping')}
+                    className="form-control"
+                >
+                    <option value="0">No</option>
+                    <option value="1">Yes</option>
+                </select>
+            </div>
+
+            <div className="form-group">
+                <label className="text-muted">Quantity</label>
+                <input
+                    onChange={handleChange('quantity')}
+                    type="number"
+                    className="form-control"
+                    value={quantity}
+                />
+            </div>
+
+            <button className="" btn btn-outline-primary>Create Product</button>
+
+        </form>
+    )
 
     const { user, token } = isAuthenticated();
+
     return (
         <Layout
             title="Add a new product"
@@ -14,7 +134,7 @@ const AddProduct = () => {
         >
             <div className="row">
                 <div className="col-md-8 offset-md-2">
-                    ...
+                    {newPostForm()}
                 </div>
             </div>
         </Layout>
