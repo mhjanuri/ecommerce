1394ad2d86422cd9941be6e17aee93217ed9e9d0
hendrijanuri <hendrijanuri@gmail.com>
2020-06-27 04:26:44 +0700

    add style to jumbotron

--- front/src/core/Layout.js
+++ front/src/core/Layout.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Menu from './Menu';
+import "../styles.css";
 
 
 const Layout = ({
@@ -8,16 +9,16 @@ const Layout = ({
     className,
     children
 }) => (
-    <div>
-        <Menu />
-        <div className="jumbotron">
-            <h2>{title}</h2>
-            <p className="lead">{description}</p>
+        <div>
+            <Menu />
+            <div className="jumbotron">
+                <h2>{title}</h2>
+                <p className="lead">{description}</p>
+            </div>
+            <div className={className}>
+                {children}
+            </div>
         </div>
-        <div className={className}>
-            {children}
-        </div>
-    </div>
-);
+    );
 
 export default Layout;
