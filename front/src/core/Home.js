690dc9783865e2b5a3e37de8a148bd68c2bd9125
hendrijanuri <hendrijanuri@gmail.com>
2020-06-27 02:39:51 +0700

    card

--- front/src/core/Home.js
+++ front/src/core/Home.js
@@ -1,10 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Layout from './Layout';
+import { getProducts } from './apiCore';
+import Card from './Card';
 
-const Home = () => (
-    <Layout title="Homepage" description="Node React E-commerce App">
-        ...
-    </Layout>
-);
+const Home = () => {
+    const [productBySell, setProductBySell] = useState([]);
+    const [productByArrival, setProductByArrival] = useState([]);
+    const [error, setError] = useState([]);
+
+    const loadProductBySell = () => {
+        getProducts('sold').then(data => {
+            if (data.error) {
+                setError(data.error)
+            } else {
+                setProductBySell(data)
+            }
+        });
+    };
+
+    const loadProductByArrival = () => {
+        getProducts('createdAt').then(data => {
+            if (data.error) {
+                setError(data.error)
+            } else {
+                setProductByArrival(data)
+            }
+        });
+    };
+
+    useEffect(() => {
+        loadProductByArrival();
+        loadProductBySell();
+    }, []);
+
+    return (
+        <Layout title="Homepage" description="Node React E-commerce App">
+            <h2 className="mb-4">Best  Sellers</h2>
+            {productBySell}
+        </Layout>
+    );
+};
 
 export default Home;
---
+++ front/src/core/apiCore.js
@@ -0,0 +1,13 @@
+import { API_URL } from '../config';
+
+export const getProducts = (sortBy) => {
+    return fetch(`${API_URL}/products?sortBy${sortBy}&order=desc&limit=6`, {
+        method: "GET"
+    })
+        .then(response => {
+            return response.json()
+        })
+        .catch(error => {
+            console.log(error);
+        });
+};
