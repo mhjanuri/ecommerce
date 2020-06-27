ca14459a13eaa9ebf1969b62df89010cabd849cc
hendrijanuri <hendrijanuri@gmail.com>
2020-06-28 06:11:12 +0700

    filter categories

--- front/src/core/Checkbox.js
+++ front/src/core/Checkbox.js
@@ -2,14 +2,34 @@ import React, { useState, useEffect } from 'react';
 
 
 const Checkbox = ({ categories }) => {
-    return (
-        categories.map((c, i) => (
-            <li key={i} className="list-unstyled">
-                <input type="checkbox" className="form-check-input" />
-                <label className="form-check-label">{c.name}</label>
-            </li>
-        ))
-    );
-}
+    const [checked, setChecked] = useState([]);
+
+    const handleToggle = c => () => {
+        // return the first index or -1
+        const currentCategoryId = checked.indexOf(c)
+        const newCheckedCategoryId = [...checked]
+        // if currently checked was not already in checked state > push
+        // else pull/take off
+        if (currentCategoryId === -1) {
+            newCheckedCategoryId.push(c)
+        } else {
+            newCheckedCategoryId.splice(currentCategoryId, 1)
+        }
+        console.log(newCheckedCategoryId);
+        setChecked(newCheckedCategoryId)
+    };
+
+    return categories.map((c, i) => (
+        <li key={i} className="list-unstyled">
+            <input
+                onChange={handleToggle(c._id)}
+                value={checked.indexOf(c._id === -1)}
+                type="checkbox"
+                className="form-check-input"
+            />
+            <label className="form-check-label">{c.name}</label>
+        </li>
+    ));
+};
 
 export default Checkbox;
