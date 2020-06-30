93cfbc10086ae9cc2eb215ddaa6ce0bdafe8328d
hendrijanuri <hendrijanuri@gmail.com>
2020-06-30 15:06:06 +0700

    search cat

--- front/src/core/Search.js
+++ front/src/core/Search.js
@@ -1,13 +1,37 @@
 import React, { useState, useEffect } from 'react';
 import Layout from './Layout';
-import { getProducts } from './apiCore';
+import { getCategories } from './apiCore';
 import Card from './Card';
 
 
 const Search = () => {
+    const [data, setData] = useState({
+        categories: [],
+        category: '',
+        search: '',
+        results: [],
+        searched: false
+    });
+
+    const { categories, category, search, searched, results } = data;
+
+    const loadCategories = () => {
+        getCategories().then(data => {
+            if (data.error) {
+                console.log(data.error);
+            } else {
+                setData({ ...data, categories: data })
+            }
+        });
+    };
+
+    useEffect(() => {
+        loadCategories()
+    }, []);
+
     return (
         <div>
-            <h2>Search Bar</h2>
+            <h2>Search Bar {JSON.stringify(categories)}</h2>
         </div>
     );
 }
