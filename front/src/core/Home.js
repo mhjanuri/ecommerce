2db3ab50a87ca59602fd2cdce7bf6e7fdf74bfe2
hendrijanuri <hendrijanuri@gmail.com>
2020-06-30 12:27:14 +0700

    search component

--- front/src/core/Home.js
+++ front/src/core/Home.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import Layout from './Layout';
 import { getProducts } from './apiCore';
 import Card from './Card';
+import Search from './Search';
 
 const Home = () => {
     const [productBySell, setProductBySell] = useState([]);
@@ -39,6 +40,7 @@ const Home = () => {
             description="Node React E-commerce App"
             className="container-fluid"
         >
+            <Search />
             <h2 className="mb-4">New Arrival</h2>
             <div className="row">
                 {productByArrival.map((product, i) => (
---
+++ front/src/core/Search.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from 'react';
+import Layout from './Layout';
+import { getProducts } from './apiCore';
+import Card from './Card';
+
+
+const Search = () => {
+    return (
+        <div>
+            <h2>Search Bar</h2>
+        </div>
+    );
+}
+
+export default Search;
