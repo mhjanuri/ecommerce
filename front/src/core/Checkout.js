36108082ea0f3f8d1c617df72f94855243c37bcf
hendrijanuri <hendrijanuri@gmail.com>
2020-07-02 16:11:05 +0700

    payment frontend

--- front/src/core/Checkout.js
+++ front/src/core/Checkout.js
@@ -49,24 +49,52 @@ const Checkout = ({ products }) => {
             );
     };
 
+    const buy = () => {
+        // send the nonce to your server 
+        // nonce = data.instance.requestPaymentMethod()
+        let nonce;
+        let getNonce = data.instance.requestPaymentMethod()
+            .then(data => {
+                console.log(data);
+                nonce = data.nonce;
+                // once we have nonce (card typr, card number), send nonce as 'paymentMethodNonce'
+                // and also total to be charged
+                console.log('send nonce and total to process: ', nonce, getTotal(products));
+
+            })
+            .catch(error => {
+                console.log('dropIn error: ', error);
+                setData({ ...data, error: error.message });
+            });
+    };
+
     const showDropIn = () => (
-        <div>
+        <div onBlur={() => setData({ ...data, error: "" })}>
             {data.clientToken !== null && products.length > 0 ? (
                 <div>
                     <DropIn
                         options={{ authorization: data.clientToken }}
                         onInstance={instance => (data.instance = instance)}
                     />
-                    <button className="btn btn-success">Checkout</button>
+                    <button onClick={buy} className="btn btn-success">Pay</button>
                 </div>
             ) : null}
         </div>
     );
 
+    const showError = error => (
+        <div
+            className="alert alert-danger"
+            style={{ display: error ? '' : 'none' }}
+        >
+            {error}
+        </div>
+    )
+
     return (
         <div>
             <h2>Total: ${getTotal()}</h2>
-
+            {showError(data.error)}
             {showCheckout()}
         </div>
     );
