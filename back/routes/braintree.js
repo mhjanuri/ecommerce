2509cd5e325107d53dc4523d9b50748c9d297b6d
hendrijanuri <hendrijanuri@gmail.com>
2020-07-02 17:38:45 +0700

    payment backend

--- back/routes/braintree.js
+++ back/routes/braintree.js
@@ -2,13 +2,14 @@ const express = require('express');
 const router = express.Router();
 
 // controllers
-const { requireSignin, isAuth } = require('../controllers/auth')
+const { requireSignin, isAuth } = require('../controllers/auth');
 const { userById } = require('../controllers/user');
-const { generateToken } = require('../controllers/braintree');
+const { generateToken, processPayment } = require('../controllers/braintree');
 
 // Routes
 router.get('/braintree/getToken/:userId', requireSignin, isAuth, generateToken);
+router.post('/braintree/payment/:userId', requireSignin, isAuth, processPayment);
 
 router.param("userId", userById);
 
-module.exports = router
+module.exports = router;
