7075472724b7da237fae485c3a221da0b4bcab1a
hendrijanuri <hendrijanuri@gmail.com>
2020-07-06 16:02:51 +0700

    frontend update order status

--- front/src/admin/apiAdmin.js
+++ front/src/admin/apiAdmin.js
@@ -80,3 +80,21 @@ export const getStatusValues = (userId, token) => {
             console.log(error);
         });
 };
+
+export const updateOrderStatus = (userId, token, orderId, status) => {
+    return fetch(`${API_URL}/order/${orderId}/status/${userId}`, {
+        method: "PUT",
+        headers: {
+            Accept: 'application/json',
+            "Content-type": 'application/json',
+            Authorization: `Bearer ${token}`
+        },
+        body: JSON.stringify({ status, orderId })
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(error => {
+            console.log(error);
+        });
+};
