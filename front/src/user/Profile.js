fd47f9dd575acb8b623d53aa4df79509148a4b5b
hendrijanuri <hendrijanuri@gmail.com>
2020-07-06 17:42:45 +0700

    get user info

--- front/src/user/Profile.js
+++ front/src/user/Profile.js
@@ -1,12 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Layout from '../core/Layout';
 import { isAuthenticated } from '../auth';
 import { Link } from 'react-router-dom';
+import { read, update, updateUser } from './apiUser'
 
-const Profile = () => {
+const Profile = ({ match }) => {
+    const [values, setValues] = useState({
+        name: '',
+        email: '',
+        password: '',
+        error: false,
+        success: false,
+    });
+
+    const { token } = isAuthenticated();
+    const { name, email, password, error, success } = values;
+
+    const init = (userId) => {
+        // console.log(userId)
+        read(userId, token).then(data => {
+            if (data.error) {
+                setValues({ ...values, error: true })
+            } else {
+                setValues({ ...values, name: data.name, email: data.email })
+            }
+        })
+    };
+
+    useEffect(() => {
+        init(match.params.userId);
+    }, []);
+
     return (
-        <h2>Profile</h2>
+        <Layout
+            title="Profile"
+            description="Update your profile"
+            className="container-fluid"
+        >
+            <h2 className="mb-4">Profile Update</h2>
+            {JSON.stringify(values)}
+
+        </Layout>
     );
-}
+};
 
 export default Profile;
