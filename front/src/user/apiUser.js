ac14bc1e34b2a4dcf2893ea4f6bb6f8758802ba6
hendrijanuri <hendrijanuri@gmail.com>
2020-07-06 17:22:52 +0700

    api method for update profile

---
+++ front/src/user/apiUser.js
@@ -0,0 +1,47 @@
+import { API_URL } from '../config';
+
+export const read = (userId, token) => {
+    return fetch(`${API_URL}/user/${userId}`, {
+        method: "GET",
+        headers: {
+            Accept: 'application/json',
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`
+        }
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(error => {
+            console.log(error);
+        });
+};
+
+export const update = (userId, token, user) => {
+    return fetch(`${API_URL}/user/${userId}`, {
+        method: "PUT",
+        headers: {
+            Accept: 'application/json',
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`
+        },
+        body: JSON.stringify(user)
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(error => {
+            console.log(error);
+        });
+};
+
+export const updateUser = (user, next) => {
+    if (typeof window !== 'undefined') {
+        if (localStorage.getItem('jwt')) {
+            let auth = JSON.parse(localStorage.getItem('jwt'));
+            auth.user = user;
+            localStorage.setItem('jwt', JSON.stringify(auth));
+            next();
+        }
+    }
+};
